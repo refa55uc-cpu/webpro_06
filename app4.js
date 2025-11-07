72f04bb2817e77dccd2475d359c31ec94f43b312
Masato Suzuki <refa55uc@gmail.com>
2025-11-07 12:32:44 +0900

    第8回変更

--- app4.js
+++ app4.js
@@ -7,33 +7,74 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/hello1", (req, res) => {
   const message1 = "Hello world";
   const message2 = "Bon jour";
-  res.render('show', { greet1:message1, greet2:message2});
+  res.render('show', { greet1: message1, greet2: message2 });
 });
 
 app.get("/hello2", (req, res) => {
-  res.render('show', { greet1:"Hello world", greet2:"Bon jour"});
+  res.render('show', { greet1: "Hello world", greet2: "Bon jour" });
 });
 
 app.get("/icon", (req, res) => {
-  res.render('icon', { filename:"./public/Apple_logo_black.svg", alt:"Apple Logo"});
+  res.render('icon', { filename: "./public/Apple_logo_black.svg", alt: "Apple Logo" });
 });
 
-app.get("/omikuji1", (req, res) => {
-  const num = Math.floor( Math.random() * 6 + 1 );
-  let luck = '';
-  if( num==1 ) luck = '大吉';
-  else if( num==2 ) luck = '中吉';
+app.get("/english", (req, res) => {
+  res.render('show', { greet1: "Good Morning", greet2: "good morning" });
+});
 
-  res.send( '今日の運勢は' + luck + 'です' );
+app.get("/yonedu", (req, res) => {
+  res.render('show', { greet1: "夢ならばどれほど良かったdeath shot", greet2: "いまだにあなたのこと you maybe kill" });
+});
+
+
+app.get("/omikuji1", (req, res) => {
+  const num = Math.floor(Math.random() * 6 + 1);
+  let luck = '';
+  if (num == 1) luck = '大吉';
+  else if (num == 2) luck = '中吉';
+
+  res.send('今日の運勢は' + luck + 'です');
 });
 
 app.get("/omikuji2", (req, res) => {
-  const num = Math.floor( Math.random() * 6 + 1 );
+  const num = Math.floor(Math.random() * 6 + 1);
   let luck = '';
-  if( num==1 ) luck = '大吉';
-  else if( num==2 ) luck = '中吉';
+  if (num == 1) luck = '大吉';
+  else if (num == 2) luck = '中吉';
 
-  res.render( 'omikuji2', {result:luck} );
+  res.render('omikuji2', { result: luck });
+});
+
+app.get("/omikuj3", (req, res) => {
+  const num = Math.floor(Math.random() * 6 + 1);
+  let luck = '';
+  let comment = '';
+  if (num == 1) {
+    luck = '大吉';
+    comment = 'Ahh～↑↑↑💥💥真夏🌞🌴🏄🎇🎆🌺のJamboree〜〜〜〜‼️‼️レゲエ🇯🇲💃🙌🏻砂浜🌺🌺🏖🏖🌴🌞Big Wave🌊🌊🌊🌊🌊🌊🌊💥💥💥 '
+  }
+  else if (num == 2) {
+    luck = '中吉';
+    comment = 'Wow wo‼️釘パンチ👊💥Wow wo‼️連発で‼️👊👊👊👊👊  ガッツガッツリガッツ‼️どんな夢💭も食える⁉️Wow wo‼️俺だけの🏋️Wow wo‼️フルコース🍽👨‍🍳ガッツガッツリガッツ‼️集めるのさ🤲世界を🌎皿に🍽乗せて‼️Wow wow wow 山を食え‼️🗣️🗻🍴Wow wow wow トリコ‼️Wow wow wow 海を食え‼️🗣️🌊🍴Wow wow wow トリ‼️トリ‼️トリ‼️トリコ‼️ '
+  }
+  else if (num == 3) {
+    luck = '小吉';
+    comment = 'うおw';
+  }
+  else if (num == 4) {
+    luck = '末吉';
+    comment = 'う，うおw';
+  }
+  else if (num == 5) {
+    luck = '凶';
+    comment = '身構えている時に死神は来ないものだ';
+  }
+  else if (num == 6) {
+    luck = '大凶';
+    comment = '🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹🥹';
+  }
+
+  res.render('omikuji3', { result: luck, comment: comment });
 });
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
--- app6.js
+++ app6.js
@@ -5,55 +5,102 @@ app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 
+let station = [
+  { id: 1, code: "JE01", name: "東京駅" },
+  { id: 2, code: "JE07", name: "舞浜駅" },
+  { id: 3, code: "JE12", name: "新習志野駅" },
+  { id: 4, code: "JE13", name: "幕張豊砂駅" },
+  { id: 5, code: "JE14", name: "海浜幕張駅" },
+  { id: 6, code: "JE05", name: "新浦安駅" },
+];
+
+app.get("/keiyo", (req, res) => {
+  // 本来ならここにDBとのやり取りが入る
+  res.render('db1', { data: station });
+});
+
+let station2 = [
+  { id: 1, code: "JE01", name: "東京駅" },
+  { id: 2, code: "JE07", name: "舞浜駅" },
+  { id: 3, code: "JE12", name: "新習志野駅" },
+  { id: 4, code: "JE13", name: "幕張豊砂駅" },
+  { id: 5, code: "JE14", name: "海浜幕張駅" },
+  { id: 6, code: "JE05", name: "新浦安駅" },
+];
+
+app.get("/keiyo2", (req, res) => {
+  // 本来ならここにDBとのやり取りが入る
+  res.render('db2', { data: station2 });
+});
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/janken.html");
+// });
+
+// app.get("/test", (req, res) => {
+//   const value = req.query.radio;
+// });
+
 app.get("/hello1", (req, res) => {
   const message1 = "Hello world";
   const message2 = "Bon jour";
-  res.render('show', { greet1:message1, greet2:message2});
+  res.render('show', { greet1: message1, greet2: message2 });
 });
 
 app.get("/hello2", (req, res) => {
-  res.render('show', { greet1:"Hello world", greet2:"Bon jour"});
+  res.render('show', { greet1: "Hello world", greet2: "Bon jour" });
 });
 
 app.get("/icon", (req, res) => {
-  res.render('icon', { filename:"./public/Apple_logo_black.svg", alt:"Apple Logo"});
+  res.render('icon', { filename: "./public/Apple_logo_black.svg", alt: "Apple Logo" });
 });
 
 app.get("/omikuji1", (req, res) => {
-  const num = Math.floor( Math.random() * 6 + 1 );
+  const num = Math.floor(Math.random() * 6 + 1);
   let luck = '';
-  if( num==1 ) luck = '大吉';
-  else if( num==2 ) luck = '中吉';
+  if (num == 1) luck = '大吉';
+  else if (num == 2) luck = '中吉';
 
-  res.send( '今日の運勢は' + luck + 'です' );
+  res.send('今日の運勢は' + luck + 'です');
 });
 
 app.get("/omikuji2", (req, res) => {
-  const num = Math.floor( Math.random() * 6 + 1 );
+  const num = Math.floor(Math.random() * 6 + 1);
   let luck = '';
-  if( num==1 ) luck = '大吉';
-  else if( num==2 ) luck = '中吉';
+  if (num == 1) luck = '大吉';
+  else if (num == 2) luck = '中吉';
 
-  res.render( 'omikuji2', {result:luck} );
+  res.render('omikuji2', { result: luck });
 });
 
 app.get("/janken", (req, res) => {
-  let hand = req.query.hand;
-  let win = Number( req.query.win );
-  let total = Number( req.query.total );
-  console.log( {hand, win, total});
-  const num = Math.floor( Math.random() * 3 + 1 );
+  const value = req.query.radio;
+  let hand = req.query.radio;
+  let win = Number(req.query.win) || 0;
+  let total = Number(req.query.total) || 0;
+  console.log({ hand, win, total });
+  const num = Math.floor(Math.random() * 3 + 1);
   let cpu = '';
   let judgement = '';
-  if( num==1 ) cpu = 'グー';
-  else if( num==2 ) cpu = 'チョキ';
+  if (num == 1) cpu = 'グー';
+  else if (num == 2) cpu = 'チョキ';
   else cpu = 'パー';
   // ここに勝敗の判定を入れる
   // 以下の数行は人間の勝ちの場合の処理なので，
   // 判定に沿ってあいこと負けの処理を追加する
-  judgement = '勝ち';
-  win += 1;
-  total += 1;
+  if ((hand == 'グー' && cpu == 'チョキ') || (hand == 'チョキ' && cpu == 'パー') || (hand == 'パー' && cpu == 'グー')) {
+    judgement = '勝ち';
+    win += 1;
+    total += 1;
+  }
+  else if (hand == cpu) {
+    judgement = 'あいこ';
+    total += 1;
+  }
+  else {
+    judgement = '負け';
+    total += 1;
+  }
   const display = {
     your: hand,
     cpu: cpu,
@@ -61,7 +108,7 @@ app.get("/janken", (req, res) => {
     win: win,
     total: total
   }
-  res.render( 'janken', display );
+  res.render('janken', display);
 });
 
 app.get("/get_test", (req, res) => {
@@ -72,22 +119,22 @@ app.get("/get_test", (req, res) => {
 
 app.get("/add", (req, res) => {
   console.log("GET");
-  console.log( req.query );
-  const num1 = Number( req.query.num1 );
-  const num2 = Number( req.query.num2 );
-  console.log( num1 );
-  console.log( num2 );
-  res.json( {answer: num1+num2} );
+  console.log(req.query);
+  const num1 = Number(req.query.num1);
+  const num2 = Number(req.query.num2);
+  console.log(num1);
+  console.log(num2);
+  res.json({ answer: num1 + num2 });
 });
 
 app.post("/add", (req, res) => {
   console.log("POST");
-  console.log( req.body );
-  const num1 = Number( req.body.num1 );
-  const num2 = Number( req.body.num2 );
-  console.log( num1 );
-  console.log( num2 );
-  res.json( {answer: num1+num2} );
+  console.log(req.body);
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+  console.log(num1);
+  console.log(num2);
+  res.json({ answer: num1 + num2 });
 });
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
